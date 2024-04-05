9266e7bc95bf7f3057a07fe39066ed3be5e83d0e
Gil <gilvicentevieira@gmail.com>
2024-04-05 21:50:04 +0100

    adding App comp

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <the-header></the-header>
+  <the-main></the-main>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue'
+import TheMain from './components/TheMain.vue'
+
+export default {
+  components: {
+    'the-header': TheHeader,
+    'the-main': TheMain
+  }
+}
+
+</script>
+
+<style>
+* {
+  margin: 0;
+}
+
+</style>./components/TheHeader.vue
