917ac2550bb5d7e649065f65247141de53bbc369
Gil <gilvicentevieira@gmail.com>
2024-04-05 21:50:13 +0100

    adding the header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>RememberMe</h1>
+  </header>
+</template>
+
+<style scoped>
+  header{
+    background-color: rgb(114, 32, 81);
+    height: 12vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  h1{
+    color: white
+  }
+</style>
